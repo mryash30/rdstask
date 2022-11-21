deccfa52c2fe49486fdc881fe5097db16dca4684
mryash30 <45002924+mryash30@users.noreply.github.com>
2022-11-22 01:46:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import boto3
+
+dms = boto3.client('rds')
+
+response = dms.describe_db_instances()
+
+#print(response)
+
+for i in response["DBInstances"]:
+    print(i["DBInstanceIdentifier"])
+    print(i["DBInstanceStatus"])
+    print("\n")
+    if (i["DBInstanceStatus"]=="available"):
+        dms.stop_db_instance(DBInstanceIdentifier=i["DBInstanceIdentifier"])
+
+#for dbinst in response:
+#    print()
